158896c159d6c2970d44d2ecea4475100198bf25
jayanthivarri <jayanthivarri@gmail.com>
2026-08-10 15:49:38 +0530

    database

--- backend/app/database/database.py
+++ backend/app/database/database.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-
+os.makedirs("./data", exist_ok=True)
 engine = create_engine(
     DATABASE_URL,
     connect_args={"check_same_thread": False}
