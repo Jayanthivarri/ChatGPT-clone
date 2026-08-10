da7a94c0274b9b1a3e372ce44cc00948f914573e
jayanthivarri <jayanthivarri@gmail.com>
2026-08-10 16:33:59 +0530

    fix cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,7 +20,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173",
+       "https://chatgpt-clone-3-o44o.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
